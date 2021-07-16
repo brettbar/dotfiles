4c86f21f417ff4fdf3b6813c78d6a5fb7870cb06
Brett Barinaga <brettbar@live.com>
2021-07-16 08:28:48 -0700

    fixed annoying lspsaga spam:

--- nvim/lua/config/lspsaga.lua
+++ nvim/lua/config/lspsaga.lua
@@ -2,8 +2,8 @@ local saga = require 'lspsaga'
 saga.init_lsp_saga {
   error_sign = '',
   warn_sign = '',
-  hint_sign = '',
-  infor_sign = '',
+  --hint_sign = '',
+  --infor_sign = '',
   code_action_icon = '',
   border_style = "round",
 }
