5676a28a9fde4520b44eff94375e606574daac74
Brett Barinaga <brettbar@live.com>
2021-08-12 14:58:51 -0700

    push fira

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -2,10 +2,8 @@ local nvim_lsp = require("lspconfig")
 local map = require("utils").map
 require("flutter-tools").setup{}
 require("lsp_signature").setup()
-<<<<<<< HEAD
 
 nvim_lsp.omnisharp.setup{}
-=======
 local function make_config()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -26,7 +24,6 @@ local function setup_servers()
 end
 setup_servers()
 
->>>>>>> 51859439caffe90930c1a6692b9ba61cbb066193
 nvim_lsp.gopls.setup{}
 nvim_lsp.clangd.setup{}
 
@@ -84,13 +81,13 @@ nvim_lsp.tsserver.setup{
 vim.lsp.diagnostic["textDocument/publishDiagnostics"] = function() end
 
 map('n','gD','<cmd>lua vim.lsp.buf.definition()<CR>')
---map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
---map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
---map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
---map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
---map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
---map('n','gp','<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
---map('n','gn','<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+map('n','gp','<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n','gn','<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
 
 map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
 map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
@@ -102,17 +99,17 @@ map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
 map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
 
-map('n','gd','<cmd>:Lspsaga preview_definition<CR>')
-map('n', 'gh', '<cmd>:Lspsaga lsp_finder<CR>')
-map('n', '<leader>gc', '<cmd>:Lspsaga code_action<CR>')
-map('n', '<leader>gc', '<cmd>:Lspsaga range_code_action<CR>')
-map('n','K','<cmd>:Lspsaga hover_doc<CR>')
-map('n', '<C-f>', '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(1)<CR>')
-map('n', '<C-b>', '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(-1)<CR>')
-map('n','gs','<cmd>:Lspsaga signature_help<CR>')
-map('n','gR','<cmd>:Lspsaga rename<CR>')
-map('n','gp','<cmd>:Lspsaga diagnostic_jump_prev<CR>')
-map('n','gn','<cmd>:Lspsaga diagnostic_jump_next<CR>')
+--map('n','gd','<cmd>:Lspsaga preview_definition<CR>')
+--map('n', 'gh', '<cmd>:Lspsaga lsp_finder<CR>')
+--map('n', '<leader>gc', '<cmd>:Lspsaga code_action<CR>')
+--map('n', '<leader>gc', '<cmd>:Lspsaga range_code_action<CR>')
+--map('n','K','<cmd>:Lspsaga hover_doc<CR>')
+--map('n', '<C-f>', '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(1)<CR>')
+--map('n', '<C-b>', '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(-1)<CR>')
+--map('n','gs','<cmd>:Lspsaga signature_help<CR>')
+--map('n','gR','<cmd>:Lspsaga rename<CR>')
+--map('n','gp','<cmd>:Lspsaga diagnostic_jump_prev<CR>')
+--map('n','gn','<cmd>:Lspsaga diagnostic_jump_next<CR>')
 
 
 --" LSP config (the mappings used in the default file don't quite work right)
