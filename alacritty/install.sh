10c59380484c5b7c74f88583ce25c275eb92f167
Brett Barinaga <brettbar@live.com>
2021-09-22 17:17:06 -0700

    gather.sh

--- alacritty/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-
---
+++ scripts/displays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# xrandr only works in X11 sessions, not Wayland
+[ "$XDG_SESSION_TYPE" = x11 ] || exit 0
+#xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
+#xrandr --addmode VGA1 1368x768_60.00
+#xrandr --output VGA1 --mode 1368x768_60.00
+sleep 1
+autorandr --change
+
+feh --bg-fill /home/brettbar/Pictures/Wallpapers/bladerunner
