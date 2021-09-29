20d399eabec7437ef6147009451730fe6e8a376a
Brett Barinaga <brettbar@live.com>
2021-09-29 14:56:10 -0700

    obsidian conf

--- gather.sh
+++ gather.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-cp $HOME/.config/alacritty/alacritty.yml $HOME/dotfiles/alacritty/alacritty.yml
-cp $HOME/.config/i3/config               $HOME/dotfiles/i3/config
-cp $HOME/.config/lvim/config.lua         $HOME/dotfiles/lvim/config.lua
-cp $HOME/.config/polybar/config          $HOME/dotfiles/polybar/config
-cp $HOME/.config/polybar/launch.sh       $HOME/dotfiles/polybar/launch.sh
+cp $HOME/.config/alacritty/alacritty.yml    $HOME/dotfiles/alacritty/alacritty.yml
+cp $HOME/.config/i3/config                  $HOME/dotfiles/i3/config
+cp $HOME/.config/lvim/config.lua            $HOME/dotfiles/lvim/config.lua
+cp $HOME/.config/polybar/config             $HOME/dotfiles/polybar/config
+cp $HOME/.config/polybar/launch.sh          $HOME/dotfiles/polybar/launch.sh
 
-cp $HOME/scripts/*                       $HOME/dotfiles/scripts/ -r 
-cp $HOME/.tmux.conf                      $HOME/dotfiles/tmux/.tmux.conf
-cp $HOME/.bashrc                         $HOME/dotfiles/scripts/.bashrc
+cp $HOME/scripts/*                          $HOME/dotfiles/scripts/ -r 
+cp $HOME/.tmux.conf                         $HOME/dotfiles/tmux/.tmux.conf
+cp $HOME/.bashrc                            $HOME/dotfiles/scripts/.bashrc
+cp $HOME/notes/.obsidian/snippets/font.css  $HOME/dotfiles/obsidian/font.css
