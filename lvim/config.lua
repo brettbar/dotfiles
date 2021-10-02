899a17b384b09855e44a47d6c03b421dcf5cd849
Brett Barinaga <brettbar@live.com>
2021-10-01 21:59:42 -0700

    hide certain ftypes

--- lvim/config.lua
+++ lvim/config.lua
@@ -80,6 +80,7 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.ignore = { "*.obj", "*.os", }
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = "maintained"
